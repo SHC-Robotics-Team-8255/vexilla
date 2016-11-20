85d15f281c3d291fc7bc07c530f07087a9728bd3
Greg Herlein <gherlein@herlein.com>
2016-11-20 15:14:06 -0800

    first

--- install.sh
+++
@@ -1,9 +0,0 @@
-mkdir src
-cd src
-git clone git@github.com:gherlein/ChibiOS.git
-git clone git@github.com:gherlein/convex-vexilla.git
-git clone git@github.com:gherlein/example-vex.git
-wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q2-update/+download/gcc-arm-none-eabi-5_4-2016q2-20160622-linux.tar.bz2
-tar xjvf gcc-arm-none-eabi-5_4-2016q2-20160622-linux.tar.bz2
-rm gcc-arm-none-eabi-5_4-2016q2-20160622-linux.tar.bz2
-
